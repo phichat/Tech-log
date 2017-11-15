d0b87308db44e705f4fdb9e5c6016c4987932116
Phichat K <phichat_dk@hotmail.com>
2017-11-15 18:15:31 +0700

    update file

---
+++ excise/public_html/lib/notice-custom/js/main.js
@@ -0,0 +1,19 @@
+function onToggleCardBody(e) {
+    $(e).find('i.material-icons').text(function (i, text) {
+        return text == "arrow_drop_down" ? "arrow_drop_up" : "arrow_drop_down";
+    })
+    var divCard = $(e).closest('div.card')
+    divCard.find('div.body').slideToggle()
+}
+
+// checkbox, col(index)
+function checkedBox(e, col) {
+    //Check All checkbox in table
+    var table = $(e).closest('table');
+    $(table).find('tbody tr').each(function (i, ele) {
+        $(ele)
+            .find('td:eq(' + col + ') input[type=checkbox]')
+            .prop('checked', $(e).prop('checked'));
+    });
+
+}

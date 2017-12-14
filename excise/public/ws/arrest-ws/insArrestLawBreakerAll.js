079657b858f02a95b251946090a6d5aceb350c0c
Phichat K <phichat_dk@hotmail.com>
2017-12-14 11:51:04 +0700

    update Arrest ins

--- excise/public/ws/arrest-ws/insArrestLawBreakerAll.js
+++ excise/public/ws/arrest-ws/insArrestLawBreakerAll.js
@@ -1,3 +1,28 @@
 function insArrestLawbrakerAll() {
     var url = 'http://103.233.193.62/ManageLawbreaker/insArrestLawbrakerAll'
+
+    // EntityType					    ประเภทบุคคลผู้กระทำผิด 1.บุคคลธรรมดา 2.นิติบุคคล		
+    // CompanyName					    หน่วยงานนิติบุคคลซึ่งกระทำผิด		
+    // CoperationCode					หมายเลขจดทะเบียนนิติบุคคล		
+    // TitleCode		    M		Y	รหัสคำนำหน้าชื่อ		
+    // TitleName		    M			คำนำหน้าชื่อ		
+    // FirstName		    M			"ชื่อผู้กระทำผิด/ผู้แทนผู้กระทำผิด"		
+    // MiddleName		    M			ชื่อกลางผู้กระทำผิด/ผู้แทนผู้กระทำผิด  		
+    // LastName		        M			นามสกุลผู้กระทำผิด/ผู้แทนผู้กระทำผิด  		
+    // OtherName		    O			ชื่ออื่นๆ 		
+    // NationalityCode		M			รหัสสัญาติ		
+    // NationalityName		M			สัญาติ		
+    // Career		        O			อาชีพ		
+    // ExciseRegNO		    O			เลขทะเบียนสรรพสามิต		
+    // RaceCode		        M			รหัสเชื้อชาติ		
+    // RaceName		        M			เชื้อชาติ	
+    // BirthDate		    O			วันเกิด		
+    // FatherName		    O			ชื่อ บิดา ผู้กระทำผิด		
+    // MotherName		    O			ชื่อ มารดา ผู้กระทำผิด		
+    // Address		        O			ที่อยู่		
+    // Village		        O			หมู่ที่		
+    // Alley		        O			ตรอก/ซอย		
+    // Road		            O			ถนน		
+    // SubdistrictCode		M		Y	รหัสตำบล			
+    
 }
--- excise/public/ws/arrest-ws/insArrestProductlistByAll.js
+++ excise/public/ws/arrest-ws/insArrestProductlistByAll.js
@@ -1,3 +1,12 @@
 function insArrestProductlistByAll(){
     var url = 'http://103.233.193.62/ManageProductlist/insArrestProductlistByAll'
+
+    // ProductListId		    M	Y		Runing Number		
+    // GroupCode		        M		Y	กลุ่มสินค้า		
+    // GroupName			        		สินค้า		
+    // NoticeCode		        M		Y	อ้างอิงเอกสารใบแจ้งความนำจับ FK Table : ops_notice		
+    // LawsuitCode		        M		Y	"อ้างอิงเลขที่คดี
+    // FK Table : ops_lawsuit"		
+    // CreationDate		        M			วันที่สร้างรายการ		
+    // CreateUser		        M			User ที่สร้างรายการ		
 }

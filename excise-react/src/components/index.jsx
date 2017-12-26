10452f8800d106534d76b78b7b9570c8fec1607b
Phichat K <phichat_dk@hotmail.com>
2017-12-26 09:39:40 +0700

    new commit

--- excise-react/src/components/index.jsx
+++
@@ -1,5 +0,0 @@
-export { default as App } from './App'
-export { default as Header } from './Header'
-export { default as Content } from './Content'
-export { default as Notice } from './Notice'
-export { default as Arrest } from './Arrest'

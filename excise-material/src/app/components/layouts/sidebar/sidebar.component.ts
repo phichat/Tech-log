0706894d4a93bcc222000b6c0da193e6f40bedfd
Phichat K <phichat_dk@hotmail.com>
2018-04-22 20:31:43 +0700

    commit

--- excise-material/src/app/components/layouts/sidebar/sidebar.component.ts
+++ excise-material/src/app/components/layouts/sidebar/sidebar.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
+declare var jQuery: any;
 
 @Component({
   selector: 'ma-sidebar',
   templateUrl: './sidebar.component.html'
 })
-export class SidebarComponent {
+export class SidebarComponent implements AfterViewInit {
 
   constructor(private router: Router) {
   }
 
+  ngAfterViewInit() {
+    jQuery('#sidebarnav').metisMenu();
+
+    if (jQuery('body').hasClass('fixed-sidebar')) {
+      jQuery('.sidebar-collapse').slimscroll({
+        height: '100%'
+      });
+    }
+  }
 
   activeRoute(routename: string): boolean {
     return this.router.url.indexOf(routename) > -1;
